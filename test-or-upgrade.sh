a0efde477f63cdc51bebde6fdf7b89fa81ea92d7
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-06-26 22:46:24 -0400

    look if package.json has been modified after upgrades

--- test-or-upgrade.sh
+++ test-or-upgrade.sh
@@ -7,6 +7,13 @@ if [ "$TRAVIS_EVENT_TYPE" = "cron" ]; then
   npm i -g next-update
   next-update -m ggit --allow patch
   git status
+  # if package.json is modified we have
+  # new upgrades
+  if git diff --name-only | grep package.json > /dev/null; then
+    echo "There are new versions of dependencies 💪"
+  else
+    echo "No new versions found ✋"
+  fi
 else
   echo "Not a cron job, normal test"
   npm test
