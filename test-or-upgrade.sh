c64f7344a850c7adddd2f9468eb4ce92877970aa
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-06-26 23:28:48 -0400

    try just upgrades

--- test-or-upgrade.sh
+++ test-or-upgrade.sh
@@ -2,20 +2,20 @@
 
 set -o xtrace
 
-if [ "$TRAVIS_EVENT_TYPE" = "cron" ]; then
-  echo "Upgrading dependencies using next-update"
-  npm i -g next-update
-  next-update -m ggit --allow patch
-  git status
-  # if package.json is modified we have
-  # new upgrades
-  if git diff --name-only | grep package.json > /dev/null; then
-    echo "There are new versions of dependencies 💪"
-  else
-    echo "No new versions found ✋"
-  fi
+# if [ "$TRAVIS_EVENT_TYPE" = "cron" ]; then
+echo "Upgrading dependencies using next-update"
+npm i -g next-update
+next-update -m ggit --allow patch
+git status
+# if package.json is modified we have
+# new upgrades
+if git diff --name-only | grep package.json > /dev/null; then
+  echo "There are new versions of dependencies 💪"
 else
-  echo "Not a cron job, normal test"
-  npm test
+  echo "No new versions found ✋"
 fi
+# else
+#   echo "Not a cron job, normal test"
+#   npm test
+# fi
 
