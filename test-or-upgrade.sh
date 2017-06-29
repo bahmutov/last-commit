b1fac33e16cfbf514cd7a40faf1265627aa3ad59
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-06-28 21:48:42 -0400

    check all updates not just the latest

--- test-or-upgrade.sh
+++ test-or-upgrade.sh
@@ -5,7 +5,7 @@ set -o xtrace
 if [ "$TRAVIS_EVENT_TYPE" = "cron" ]; then
   echo "Upgrading dependencies using next-update"
   npm i -g next-update
-  next-update --allow patch
+  next-update --allow patch --latest false
   git status
   # if package.json is modified we have
   # new upgrades
@@ -14,7 +14,7 @@ if [ "$TRAVIS_EVENT_TYPE" = "cron" ]; then
     git add package.json
     git config --global user.email "next-update@ci.com"
     git config --global user.name "next-update"
-    git commit -m "chore(deps): upgraded dependencies with next-update"
+    git commit -m "chore(deps): upgrade dependencies using next-update"
     # push back to GitHub using token
     git remote remove origin
     # TODO read origin from package.json
